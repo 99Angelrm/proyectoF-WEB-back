642093622b874c692e0b17ff144c70a8f54385db
99angelrm <99angelrm@gmail.com>
2021-11-30 09:20:18 -0600

    80 sí es

--- controllers/cliente.js
+++ controllers/cliente.js
@@ -3,21 +3,21 @@ const Plato = require("../models/plato");
 const mongoose = require("mongoose");
 
 //CRUD
-// CREATE AND ORDER 
-exports.postAgregarOrden = async (req,res)=>{
-    const orden = new Orden(req.body)
-    orden._id = new mongoose.Types.ObjectId()
-    try{
+// CREATE AND ORDER
+exports.postAgregarOrden = async (req, res) => {
+    const orden = new Orden(req.body);
+    orden._id = new mongoose.Types.ObjectId();
+    try {
         //Agregar el documento a la colección
-        await orden.save()
-        console.log(orden)
-        console.log("orden registrado")
-        res.send({operacion:"correcta"})
-    }catch(err){
-        console.log(err)
-        res.send({operacion: "incorrecta"})
+        await orden.save();
+        console.log(orden);
+        console.log("orden registrado");
+        res.send({ operacion: "correcta" });
+    } catch (err) {
+        console.log(err);
+        res.send({ operacion: "incorrecta" });
     }
-}
+};
 // READ PLATILLOS
 exports.getObtenerPlatillos = async (req, res) => {
     try {
@@ -28,27 +28,36 @@ exports.getObtenerPlatillos = async (req, res) => {
     }
 };
 // UPDATE ORDER
-exports.postActualizarOrden = async(req,res)=>{
+exports.postActualizarOrden = async (req, res) => {
     //Filtro y cambio
-    try{
-        await Orden.findOneAndUpdate({nombre:req.body.nombre},{title:req.body.title})
-        console.log("Cambio realizado")
-        res.json({operacion: "correcta"})
-    }catch(err){
-        console.log(err)
-        res.json({operacion: "incorrecta"})
+    try {
+        const prev = await Orden.findOne({ nombre: req.body.nombre });
+        const neww = await Plato.findOne({ title: req.body.title });
+        await Orden.findOneAndUpdate(
+            { nombre: req.body.nombre },
+            {
+                src: neww.src,
+                title: neww.title,
+                text: neww.text,
+                ingredientes: neww.ingredientes,
+            }
+        );
+        console.log("Cambio realizado");
+        res.json({ operacion: "correcta" });
+    } catch (err) {
+        console.log(err);
+        res.json({ operacion: "incorrecta" });
     }
-}
+};
 
-//DELETE ORDER 
-exports.postBorrarOrden = async (req,res)=>{
-    await Orden.findOneAndRemove(req.body)
-    console.log("Orden eliminada")
-    res.json({operacion: "correcta"})
-}
+//DELETE ORDER
+exports.postBorrarOrden = async (req, res) => {
+    await Orden.findOneAndRemove(req.body);
+    console.log("Orden eliminada");
+    res.json({ operacion: "correcta" });
+};
 
-exports.borrar = async(req,res)=>{
-    await Orden.remove()
-    res.json({operacion: "correcta"})
-}
-
+exports.borrar = async (req, res) => {
+    await Orden.remove();
+    res.json({ operacion: "correcta" });
+};
